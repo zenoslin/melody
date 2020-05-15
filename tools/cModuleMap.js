1f396cd01d9c44667d566dcd867182ef27832248
zenoslin <zenoslin@outlook.com>
2020-05-15 16:28:22 +0800

    fix: :bug: 修复打包流程，组件路径获取

--- tools/cModuleMap.js
+++ tools/cModuleMap.js
@@ -2,19 +2,28 @@ const fs = require('fs');
 const path = require('path');
 const componentDir = './packages';
 const cModulueNames = fs.readdirSync(path.resolve(componentDir));
-const cModulueMap = cModulueNames.reduce((prev, name) => {
-    if (name === 'style') {
-        return prev;
-    }
-    prev[name.split('.')[0]] = {
-        input:
-            name.indexOf('index') > -1
-                ? `${componentDir}/${name}`
-                : `${componentDir}/${name}/index.js`,
-        output: name.split('.')[0],
-    };
-    return prev;
-}, {});
 
+const cModulueMap = cModulueNames
+    .filter(name => {
+        const stats = fs.statSync(path.resolve(`${componentDir}/${name}`));
+        let isHasIndex;
+        try {
+            fs.accessSync(path.resolve(`${componentDir}/${name}/index.js`), fs.constants.F_OK);
+            isHasIndex = true;
+        } catch (e) {
+            isHasIndex = false;
+        }
+        return (stats.isDirectory() && isHasIndex) || name === 'index.js';
+    })
+    .reduce((prev, name) => {
+        if (name === 'style') {
+            return prev;
+        }
+        prev[name.split('.')[0]] = {
+            input: name.indexOf('index') > -1 ? `${componentDir}/${name}` : `${componentDir}/${name}/index.js`,
+            output: name.split('.')[0],
+        };
+        return prev;
+    }, {});
 
 module.exports = cModulueMap;
